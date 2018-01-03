db5736e11d7d66dde2aa1f8ab142fa9282447e73
Dhanashree Dalal <dadalal09@gmail.com>
2018-01-03 16:24:02 +0530

    http to https

--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,3 +1,3 @@
-//export const baseURL = "http://localhost:3000/";
+//export const baseURL = "http://localhost:3000/"; 
 
-export const baseURL = "http://demo3587762.mockable.io/";
+export const baseURL = "https://demo3587762.mockable.io/";
